2cd71d3add2b6839d1d7117560b672225ce16ca3
Abel28 <abel.aguilera.28@gmail.com>
2023-02-18 10:01:48 -0300

    se actualiza settings tarea_1

--- tarea_1/tarea_1/settings.py
+++ tarea_1/tarea_1/settings.py
@@ -79,10 +79,13 @@ WSGI_APPLICATION = 'tarea_1.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME' : 'postgres',
+            'USER' : 'postgres',
+            'PASSWORD': '',
+            'PORT': ''
+        }
 }
 
 
